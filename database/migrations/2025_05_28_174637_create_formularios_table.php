6c0525e66ec61fa1490d445ca2cee1d7f7a3f6ce
wallacemaxters <wallacemaxters@gmail.com>
2025-06-09 14:46:40 -0300

    mudando o tipo para "text"

--- database/migrations/2025_05_28_174637_create_formularios_table.php
+++ database/migrations/2025_05_28_174637_create_formularios_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('formularios', function (Blueprint $table) {
             $table->id();
             $table->string('nome', 200);
-            $table->string('descricao', 400)->nullable();
+            $table->text('descricao')->nullable();
             $table->string('componente', 100)->nullable();
             $table->string('apresentador', 100)->nullable();
             $table->timestamps();
